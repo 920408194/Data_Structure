7f642181dd52d0734e8cf2e6f58d8384d6c26440
panda <13217322877@163.com>
2020-05-20 08:45:17 +0800

    20200520

--- text.cpp
+++ text.cpp
@@ -1,3 +1,25 @@
-#include<stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int dg(int a,int b,int c,int m){
+	int n = 0;
+	if(m==0) n++;
+	else if(m>0){
+		n+=dg(a,b,c,m-a);
+		n+=dg(a,b,c,m-b);
+		n+=dg(a,b,c,m-c);
+	}
+	
+	return n;
+}
 
+int main(int argc, char *argv[]) {
+	int a,b,c;
+	cin>>a>>b>>c;
+	int m;
+	cin >> m;
+	cout << dg(a,b,c,m);
+
+	return 0;
+}
